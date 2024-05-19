45a459d859573a06fab29070da46f00aac7edf8b
Jesus Fajardo <es.jfajardobass@gmail.com>
2024-05-19 02:04:58 +0200

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import 'primereact/resources/themes/soho-dark/theme.css'
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import InternalDialog from "./components/InternalDialog.jsx";
+
+function App() {
+    return (
+        <>
+            <InternalDialog></InternalDialog>
+        </>
+    )
+}
+
+export default App
